8c1f8466929afadfdb7e88feaa7df52df99fa0ac
danielcoker <danielcoker28@gmail.com>
2019-10-11 07:10:57 +0100

    add: Attempt to send email from Django

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'danielcoker28@gmail.com'
+EMAIL_HOST_PASSWORD = 'Danny.coker7'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
